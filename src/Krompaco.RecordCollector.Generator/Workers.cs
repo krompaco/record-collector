f2f088aedb9b8b51dc632eff7617383225820cab
Johan Kronberg <johan.kronberg@netrelations.com>
2020-04-14 23:53:24 +0200

    Starting with lab generator code.

--- src/Krompaco.RecordCollector.Generator/Workers.cs
+++ src/Krompaco.RecordCollector.Generator/Workers.cs
@@ -1,5 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Krompaco.RecordCollector.Content.IO;
+using Krompaco.RecordCollector.Content.Languages;
+using Krompaco.RecordCollector.Content.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 
 namespace Krompaco.RecordCollector.Generator
@@ -8,22 +18,44 @@ namespace Krompaco.RecordCollector.Generator
     {
         private readonly WebApplicationFactory<Web.Startup> factory;
 
+        private readonly FileService fileService;
+
         public Workers(WebApplicationFactory<Web.Startup> factory)
         {
             this.factory = factory;
+            this.fileService = new FileService("C:\\DevStuff\\github\\docsy-example\\content", new ContentCultureService(), NullLogger.Instance);
         }
 
-        [Theory]
-        [InlineData("/")]
-        [InlineData("/?whaddup=1")]
-        public async Task Request(string url)
+        [Fact]
+        public async Task GenerateStaticSite()
         {
+            var outputPath = $"c:\\DevStuff\\temp\\rc-content\\{DateTime.Now.ToString("yyyyMMddHHmmss")}";
+            Directory.CreateDirectory(outputPath);
+
             var client = this.factory.CreateClient();
 
-            var response = await client.GetAsync(url).ConfigureAwait(true);
+            var allFileModels = this.fileService.GetAllFileModels();
 
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+            var allRequestTasks = new List<Task<HttpResponseMessage>>();
+
+            foreach (var file in allFileModels.Where(x => x is SinglePage))
+            {
+                var task = client.GetAsync(file.RelativeUrl);
+                allRequestTasks.Add(task);
+            }
+
+            var responses = await Task.WhenAll(allRequestTasks).ConfigureAwait(true);
+            var i = 0;
+
+            foreach (var response in responses)
+            {
+                i++;
+                response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+                using var output = File.Create(Path.Combine(outputPath, i + ".html"));
+                using var input = await response.Content.ReadAsStreamAsync().ConfigureAwait(true);
+                input.CopyTo(output);
+            }
         }
     }
 }
