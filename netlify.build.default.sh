948654729465cba3829708d327585ce46b1b324f
Johan Kronberg <johan.kronberg@krompaco.nu>
2026-03-03 11:38:38 +0100

    Add variables to Netlify .sh script

--- netlify.build.default.sh
+++ netlify.build.default.sh
@@ -7,4 +7,9 @@ chmod u+x /tmp/dotnet-install.sh
 /tmp/dotnet-install.sh --channel 10.0
 popd
 
+export DOTNET_ROOT=/opt/buildhome/.dotnet
+export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+
+dotnet --list-sdks
+
 npm ci && npm run prodbuild && dotnet test ./src/Krompaco.RecordCollector.Generator/Krompaco.RecordCollector.Generator.csproj --logger "console;verbosity=detailed"
