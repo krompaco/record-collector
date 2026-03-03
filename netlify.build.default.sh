0ee8385be16af366b1825629f0ca1f9a06e51661
Johan Kronberg <johan.kronberg@krompaco.nu>
2026-03-03 12:12:31 +0100

    Update .sh to work with .NET 10.0

--- netlify.build.default.sh
+++ netlify.build.default.sh
@@ -4,12 +4,9 @@ set -e
 pushd /tmp
 wget https://dot.net/v1/dotnet-install.sh
 chmod u+x /tmp/dotnet-install.sh
-/tmp/dotnet-install.sh --channel 10.0
+/tmp/dotnet-install.sh --channel 10.0 --install-dir /tmp/dotnet-sdk
 popd
 
-export DOTNET_ROOT=/opt/buildhome/.dotnet
-export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+/tmp/dotnet-sdk/dotnet --version
 
-dotnet --list-sdks
-
-npm ci && npm run prodbuild && dotnet test ./src/Krompaco.RecordCollector.Generator/Krompaco.RecordCollector.Generator.csproj --logger "console;verbosity=detailed"
+npm ci && npm run prodbuild && /tmp/dotnet-sdk/dotnet test ./src/Krompaco.RecordCollector.Generator/Krompaco.RecordCollector.Generator.csproj --logger "console;verbosity=detailed"
--- netlify.build.simplecss.sh
+++ netlify.build.simplecss.sh
@@ -4,7 +4,9 @@ set -e
 pushd /tmp
 wget https://dot.net/v1/dotnet-install.sh
 chmod u+x /tmp/dotnet-install.sh
-/tmp/dotnet-install.sh --channel 10.0
+/tmp/dotnet-install.sh --channel 10.0 --install-dir /tmp/dotnet-sdk
 popd
 
-dotnet test ./src/Krompaco.RecordCollector.Generator/Krompaco.RecordCollector.Generator.csproj --logger "console;verbosity=detailed"
+/tmp/dotnet-sdk/dotnet --version
+
+/tmp/dotnet-sdk/dotnet test ./src/Krompaco.RecordCollector.Generator/Krompaco.RecordCollector.Generator.csproj --logger "console;verbosity=detailed"
